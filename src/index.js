3ce73a0cb9b934997881c04840bbb8b26eea083a
Michał Pietrucha <michal.pietrucha@off-site.pl>
2022-04-12 12:58:47 +0200

    ok

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import { defineNuxtModule } from '@nuxt/kit'
 import { join } from 'pathe'
 
 export default defineNuxtModule({
+	meta: {
+		compatibility: {
+			nuxt: '^3.0.0'
+		}
+	},
 	setup(options, nuxt) {
 		nuxt.hook('autoImports:dirs', dirs => {
 			dirs.push(join(nuxt.options.rootDir, 'store'))
